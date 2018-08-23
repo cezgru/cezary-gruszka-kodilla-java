961582a82792db31f56cb85ae5acad6c4367f933
Cezary Gruszka <cezgru@gmail.com>
2018-08-23 20:14:13 +0200

    8.2 fixed

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -5,29 +5,13 @@ import java.sql.SQLOutput;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if(b == 0){
-                throw new ArithmeticException();
-            }
-            return a / b;
-
-        } catch (ArithmeticException e) {
-            System.out.println("Hopefully you're working on the Riemann Sphere, sadly " +
-                    "defining the complex infinity in basic java is rather hard, so returning NaN," +
-                    " please refrain from dividing by zero if possible. Error: " + e);
-            return Double.NaN;
-
-        } finally {
-            System.out.println("Uhmm, good for you? Why is this even here I don't see much of a purpose for it" +
-                    ", Kodilla pls. That aside, this doesn't even require an implementation. I mean" +
-                    ", if you delete the if statement this code will literally just return " +
-                    "infinity or -infinity, which is as close as it gets for real numbers, " +
-                     "so why am I even implementing something here. Also, why I am writing comments" +
-                    "as system output expressions is beyond me.");
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
+    }
 
-}
+
 
     /**
      * This main can throw an ArithmeticException!!!
@@ -36,8 +20,26 @@ public class FirstChallenge {
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
+        double result;
 
-        double result = firstChallenge.divide(3, 0);
+        try {
+            result = firstChallenge.divide(3,0);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Hopefully you're working on the Riemann Sphere, sadly " +
+                    "defining the complex infinity in basic java is rather hard. Returning pi, as it's " +
+                    "just as valid as any other number in the reals. " +
+                    "Please refrain from dividing by zero if possible. Error: " + e);
+            result = Math.PI;
+
+        } finally {
+            System.out.println("Uhmm, good for you? Why is this even here I don't see much of a purpose for it" +
+                    ", Kodilla pls. That aside, this doesn't even require an implementation. I mean" +
+                    ", if you delete the if statement this code will literally just return " +
+                    "infinity or -infinity, which is as close as it gets for real numbers, " +
+                    "so why am I even implementing something here. Also, why I am writing comments" +
+                    "as system output expressions is beyond me.");
+        }
 
         System.out.println(result);
 
